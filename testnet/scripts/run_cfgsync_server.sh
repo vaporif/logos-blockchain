da0949cd18ff4d3f31fac4adb910e0dddc749f67
gusto <bacv@users.noreply.github.com>
2026-02-19 09:58:01 +0200

    fix(devnet): Nginx configuration for setup and run mode deployments (#2225)

--- testnet/scripts/run_cfgsync_server.sh
+++ testnet/scripts/run_cfgsync_server.sh
@@ -3,6 +3,6 @@
 set -e
 
 export CFG_SERVER_MODE="run" \
-       CFG_SERVER_STORAGE_PATH="/node-data/cfgsync/deploymen-settings.yaml"
+       CFG_SERVER_STORAGE_PATH="/node-data/cfgsync/deployment-settings.yaml"
 
 exec /usr/bin/logos-blockchain-cfgsync-server /etc/logos-blockchain/cfgsync.yaml
