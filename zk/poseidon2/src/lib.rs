85c6fecd477b15fd684a6c6e71bc88eae09b423d
thomaslavaur <82142582+thomaslavaur@users.noreply.github.com>
2026-05-07 09:47:37 +0200

    chore: clarify poseidon and add test values from the specs (#2710)

--- zk/poseidon2/src/lib.rs
+++ zk/poseidon2/src/lib.rs
@@ -7,19 +7,19 @@ pub type Poseidon2Bn254Hasher = hasher::Poseidon2Hasher;
 pub type ZkHash = Fr;
 
 pub trait Digest {
-    /// Digest takes `inputs` data and 1st apply the SAFE padding protocol and
+    /// Digest takes `inputs` data and 1st apply the 10* padding protocol and
     /// apply Poseidon2 hash function.
     ///
     /// This can be used for anything.
     fn digest(inputs: &[Fr]) -> ZkHash;
 
-    /// Compress takes exactly 2 elements as `inputs`. It doesn't apply the SAFE
+    /// Compress takes exactly 2 elements as `inputs`. It doesn't apply the
     /// padding and apply the Poseidon2 hash function.
     ///
-    /// This can only be used for protocols where the inputs is always of size
-    /// 2. In Logos blockchain it's reserved for Merkle tree computations
-    ///    including
-    /// Merkle roots and Merkle proofs.
+    /// This can only be used for protocols where the inputs are always of size
+    /// 2. In Logos blockchain it's reserved for Merkle proofs, public key
+    ///    derivation,
+    /// nullifier derivation and reward voucher derivation.
     fn compress(inputs: &[Fr; 2]) -> ZkHash;
 
     fn new() -> Self;
